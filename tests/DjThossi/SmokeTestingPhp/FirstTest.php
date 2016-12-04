f0300e3491f8e09a321cb463e14004fa0f3254be
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2016-12-04 14:01:29 +0100

    Initial commit

---
+++ tests/DjThossi/SmokeTestingPhp/FirstTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace DjThossi\SmokeTestingPhp;
+
+use PHPUnit_Framework_TestCase;
+
+class FirstTest extends PHPUnit_Framework_TestCase
+{
+    public function testFirst()
+    {
+        $this->assertTrue(true);
+    }
+}
