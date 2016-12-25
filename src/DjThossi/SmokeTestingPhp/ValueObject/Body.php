c45763fd0a0cf34df5bd15b24037b1e17728a98f
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2016-12-25 20:26:32 +0100

    Introduced value object Body

---
+++ src/DjThossi/SmokeTestingPhp/ValueObject/Body.php
@@ -0,0 +1,42 @@
+<?php
+namespace DjThossi\SmokeTestingPhp\ValueObject;
+
+use DjThossi\SmokeTestingPhp\Ensure\EnsureIsStringTrait;
+
+class Body
+{
+    use EnsureIsStringTrait;
+
+    const BODY_IS_NOT_A_STRING = 1;
+
+    /**
+     * @var string
+     */
+    private $body;
+
+    /**
+     * @param string $body
+     */
+    public function __construct($body)
+    {
+        $this->ensureBody($body);
+
+        $this->body = $body;
+    }
+
+    /**
+     * @return string
+     */
+    public function asString()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    private function ensureBody($body)
+    {
+        $this->ensureIsString('Body', self::BODY_IS_NOT_A_STRING, $body);
+    }
+}
