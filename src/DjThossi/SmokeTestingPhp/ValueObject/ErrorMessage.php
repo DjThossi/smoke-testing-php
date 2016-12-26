9a2c1d3a83eb0c5a58e745a975b557da6c4fbc90
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2016-12-26 11:52:58 +0100

    Introduce value object ErrorMessage

---
+++ src/DjThossi/SmokeTestingPhp/ValueObject/ErrorMessage.php
@@ -0,0 +1,46 @@
+<?php
+namespace DjThossi\SmokeTestingPhp\ValueObject;
+
+use DjThossi\SmokeTestingPhp\Ensure\EnsureIsNotEmptyTrait;
+use DjThossi\SmokeTestingPhp\Ensure\EnsureIsStringTrait;
+
+class ErrorMessage
+{
+    use EnsureIsStringTrait;
+    use EnsureIsNotEmptyTrait;
+
+    const MESSAGE_IS_NOT_A_STRING = 1;
+    const MESSAGE_IS_EMPTY = 2;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @param string $message
+     */
+    public function __construct($message)
+    {
+        $this->ensureMessage($message);
+
+        $this->message = $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function asString()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    private function ensureMessage($message)
+    {
+        $this->ensureIsString('Message', self::MESSAGE_IS_NOT_A_STRING, $message);
+        $this->ensureIsNotEmpty('Message', self::MESSAGE_IS_EMPTY, $message);
+    }
+}
---
+++ tests/Unit/SmokeTestingPhp/ValueObject/ErrorMessageTest.php
@@ -0,0 +1,58 @@
+<?php
+namespace Unit\DjThossi\SmokeTestingPhp\ValueObject;
+
+use DjThossi\SmokeTestingPhp\Ensure\InvalidValueException;
+use DjThossi\SmokeTestingPhp\ValueObject\ErrorMessage;
+use PHPUnit_Framework_TestCase;
+use stdClass;
+
+/**
+ * @covers \DjThossi\SmokeTestingPhp\ValueObject\ErrorMessage
+ * @covers \DjThossi\SmokeTestingPhp\Ensure\EnsureIsStringTrait
+ * @covers \DjThossi\SmokeTestingPhp\Ensure\EnsureIsNotEmptyTrait
+ */
+class ErrorMessageTest extends PHPUnit_Framework_TestCase
+{
+    public function testCanCreateInstance()
+    {
+        $errorMessage = new ErrorMessage('HelloWorld');
+        $this->assertInstanceOf(ErrorMessage::class, $errorMessage);
+    }
+
+    /**
+     * @dataProvider failingValuesProvider
+     *
+     * @param mixed $valueToTest
+     * @param int $exceptionCode
+     */
+    public function testCreateInstanceFailing($valueToTest, $exceptionCode)
+    {
+        $this->expectException(InvalidValueException::class);
+        $this->expectExceptionCode($exceptionCode);
+
+        $errorMessage = new ErrorMessage($valueToTest);
+        $this->assertInstanceOf(ErrorMessage::class, $errorMessage);
+    }
+
+    /**
+     * @return array
+     */
+    public function failingValuesProvider()
+    {
+        return [
+            'Message is Integer' => [1337, ErrorMessage::MESSAGE_IS_NOT_A_STRING],
+            'Message is Float' => [1.337, ErrorMessage::MESSAGE_IS_NOT_A_STRING],
+            'Message is true' => [true, ErrorMessage::MESSAGE_IS_NOT_A_STRING],
+            'Message is false' => [false, ErrorMessage::MESSAGE_IS_NOT_A_STRING],
+            'Message is object' => [new stdClass(), ErrorMessage::MESSAGE_IS_NOT_A_STRING],
+            'Message is empty' => ['', ErrorMessage::MESSAGE_IS_EMPTY],
+        ];
+    }
+
+    public function testCanGetAsString()
+    {
+        $message = 'HelloWorld2';
+        $errorMessage = new ErrorMessage($message);
+        $this->assertSame($message, $errorMessage->asString());
+    }
+}
