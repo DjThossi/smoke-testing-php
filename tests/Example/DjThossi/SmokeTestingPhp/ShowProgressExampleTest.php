0ac5bdab97ab8ba40f73832b42cebc6d45979fce
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2016-12-31 14:16:12 +0100

    use correct method name

--- tests/Example/DjThossi/SmokeTestingPhp/ShowProgressExampleTest.php
+++ tests/Example/DjThossi/SmokeTestingPhp/ShowProgressExampleTest.php
@@ -25,7 +25,7 @@ class ShowProgressExampleTest extends PHPUnit_Framework_TestCase
         );
     }
 
-    public function ErrorOutput(Result $result)
+    public function errorOutput(Result $result)
     {
         echo sprintf(
             "FAILED => %s \n",
